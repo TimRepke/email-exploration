aa4664743bc0a7836805cbc06239531d4e8b2428
Tim Repke <tim@repke.eu>
2017-06-15 21:21:27 +0200

    pimp data backend

--- vis/routes/data.js
+++ vis/routes/data.js
@@ -6,6 +6,7 @@ const logger = require('../utils/logger').l('data');
 let app = express();
 let entities = require('./cache/entities.json');
 let m2u = require('./cache/mail2users.json');
+let ent_ind = require('./cache/entitiy_index.json');
 
 app.get('/commons', function (req, res, next) {
     logger.debug('Received query params: ');
@@ -56,7 +57,7 @@ app.get('/commons', function (req, res, next) {
 
 app.get('/mails', function (req, res, next) {
     logger.debug('Received query params: ');
-    logger.debug(req.query);
+    //logger.debug(req.query);
 
     let uid = req.query.uid || 'users/11412795';
     let mids = (req.query.mids || '').split(',');
@@ -65,16 +66,14 @@ app.get('/mails', function (req, res, next) {
     let query = '' +
         'FOR m IN @@mails\n' +
         '  LIMIT 10\n' +
-        '  RETURN {from: m.parts[0].sender.name, to: m.parts[0].recipients[0].name, \n' +
-        '          sub: m.parts[0].subject, msg:m.parts[0].body}';
+        '  RETURN m.parts';
     if (mids.length > 0) {
         params = {mids: mids};
         query = '' +
             'FOR m IN @@mails\n' +
             '  FILTER m._id IN @mids\n' +
-            '  LIMIT 100\n' +
-            '  RETURN {from: m.parts[0].sender.name, to: m.parts[0].recipients[0].name, \n' +
-            '          sub: m.parts[0].subject, msg:m.parts[0].body}';
+           // '  LIMIT 100\n' +
+            '  RETURN m.parts';
     }
 
     console.log(query);
@@ -99,7 +98,7 @@ app.get('/mails', function (req, res, next) {
 
 app.get('/users', function (req, res, next) {
     logger.debug('Received user search request');
-    logger.debug(req.query);
+    //logger.debug(req.query);
 
     let term = req.query.term || undefined;
 
@@ -122,12 +121,59 @@ app.get('/users', function (req, res, next) {
     }, function (err) {
         logger.error(err.stack);
         res.status(500).send({error: err.stack});
-    })
+    });
+});
+
+app.get('/mids', function(req, res, next) {
+    //logger.debug(req.query);
+    let ents = (req.query.ents || '').split(',').filter(x => !!x);
+    let uids = (req.query.uids || '').split(',').filter(x => !!x);
+
+    // e2m: {ent1:  [id, id,... ], ent2:  [..], ..}
+    // m2e: {type1: [id, id,... ], type2: [..], ..}
+    function em() {
+        let em = new Set();
+
+        ents.forEach(ent => ent_ind.e2m[ent].forEach(i => em.add(ent_ind.list[i].mid)));
+        return Array.from(em);
+    }
+
+    let query = '' +
+        'FOR s IN @@edges' +
+        '   FILTER s._from IN @uids OR s._to IN @uids' +
+        '   RETURN s.mail_ids[0]';
+
+    if (ents.length === 0 && uids.length === 0) {
+        logger.trace('/mids no selectors');
+        res.send({user: -1, entities: -1});
+    } else if (ents.length > 0 && uids.length === 0){
+        let e = em();
+        logger.trace('/mids only ents, found '+e.length);
+        res.send({user: -1, entities: e});
+    } else {
+        config.db.query(
+            query, {uids: uids}
+        ).then(function (cursor) {
+            cursor.all().then(function (um) {
+                if (ents.length === 0 && uids.length > 0){
+                    logger.trace('/mids only users, found '+um.length);
+                    res.send({user: um, entities: -1});
+                } else {
+                    let e = em();
+                    logger.trace('/mids returning '+um.length+' umids and '+e.length+' emids');
+                    res.send({user: um, entities: e});
+                }
+            });
+        }, function (err) {
+            logger.error(err.stack);
+            res.status(500).send({error: err.stack});
+        });
+    }
 });
 
 app.get('/entities', function (req, res, next) {
     logger.debug('Received query params: ');
-    logger.debug(req.query);
+    //logger.debug(req.query);
 
     /*
      'DATE': 121,
@@ -145,6 +191,7 @@ app.get('/entities', function (req, res, next) {
      'FAC': 1,
      'WORK_OF_ART': 1
      */
+    /*
     let types = (req.query.types || 'ORG').split(',');
     let min_cnt = req.query.min_cnt || 1;
 
@@ -161,23 +208,68 @@ app.get('/entities', function (req, res, next) {
         return elem['cnt'] >= min_cnt;
     });
 
+    res.send(values);*/
+
+    let types = (req.query.types || 'ORG').split(',').filter(x => !!x);
+    let mids = (req.query.mids || '').split(',').filter(x => !!x);
+    let min_cnt = req.query.min_cnt || 1;
+    // {"mid":"mails/296997","entity":"Energy Market Report - 1/28/02","type":"ORG","part":0}
+    let min_cnt_cnt = 0;
+    let values = Object.values(ent_ind.list.filter(function (elem) {
+        return types.indexOf(elem['type']) >= 0 &&
+            (mids.indexOf(elem['mid']) >= 0 || mids.length === 0) &&
+            !!elem['entity'];
+    }).reduce(function (accu, e, i) {
+        if (!accu[e['entity']]) {
+            accu[e['entity']] = e;
+            accu[e['entity']]['cnt'] = 0;
+        }
+        accu[e['entity']]['cnt']++;
+        return accu;
+    }, {})).filter(function (elem) {
+        min_cnt_cnt++;
+        return elem['cnt'] >= min_cnt;
+    });
+    logger.debug('Sending '+values.length+ ' unique entities ('+min_cnt_cnt+' had t<'+min_cnt+') for types: '+types.join(',')+' and mids: '+(mids.join(',')||'[]'));
     res.send(values);
 });
 
 app.get('/graph', function (req, res, next) {
     logger.debug('Received user search request');
-    logger.debug(req.query);
+    //logger.debug(req.query);
 
-    let uid = req.query.uid || 'users/11405029';
+    let uid = req.query.uid;
+    let mids = (req.query.mids || '').split(',').filter(x => !!x);
 
-    logger.trace('start graphjs');
+    let filters = [];
+    let params = {};
+    if (!!uid) {
+        filters.push('(s._from IN @uids OR s._to IN @uids)');
+        params['uids'] = [uid];
+    }
+    if (mids.length > 0) {
+        filters.push('s.mail_ids[0] IN @mids');
+        params['mids'] = mids;
+    }
+    if (filters.length === 0) {
+        res.send({
+            edges: [],
+            nodes: [],
+            mails: []
+        });
+        return;
+    }
     let query = '' +
-        'FOR s IN @@edges' +
-        '   FILTER s._from == @uid OR s._to == @uid' +
-        '   RETURN {mail: s.mail_ids[0], f: s._from, t: s._to}';
+        'FOR s IN @@edges ' +
+        '   FILTER ' + filters.join(' AND ') +
+        '   LET f = document(s._from) '+
+        '   LET t = document(s._to) '+
+        '   RETURN {mail: s.mail_ids[0], f: s._from, t: s._to, ' +
+        '           fa: f.address, fn: f.name,  ta: t.address, tn: t.name}';
+
 
     config.db.query(
-        query, {uid: uid}
+        query, params
     ).then(function (cursor) {
         cursor.all().then(function (rel) {
             let ret = [];
@@ -225,35 +317,6 @@ app.get('/graph', function (req, res, next) {
                 nodes: nodes,
                 mails: Object.keys(mails)
             });
-        }).catch(function (err) {
-            logger.error(err.stack);
-            res.status(500).send({error: err.stack});
-        })
-    }).catch(function (err) {
-        logger.error(err.stack);
-        res.status(500).send({error: err.stack});
-    })
-});
-
-app.get('/graph_m2u_cache', function (req, res, next) {
-    logger.trace('start graphjs');
-    let idlist_query = 'FOR s in sent ' +
-        'LET f = document(s._from) ' +
-        'LET t = document(s._to) ' +
-        'RETURN {m: s.mail_ids[0], fn: f.name, fa: f.address, tn: t.name, ta: t.address, ti:s._to, fi:s._from}';
-
-    config.db.query(
-        idlist_query, {}
-    ).then(function (cursor) {
-        logger.trace('promises arrived');
-        cursor.all().then(function (all) {
-            let m = all.reduce(function (acc, s, i) {
-                if (!(s.m in acc))
-                    acc[s.m] = [];
-                acc[s.m].push({'i': i, fn: s.fn, fa: s.fa, tn: s.tn, ta: s.ta, ti: s.ti, fi: s.fi});
-                return acc;
-            }, {});
-            res.send(m);
         }).catch(function (err) {
             logger.error(err.stack);
             res.status(500).send({error: err.stack});
@@ -355,6 +418,56 @@ app.get('/star', function (req, res, next) {
 
     //res.sendFile(config.path('maps/styles/' + config.maps.style + path));
     //res.send(path);
+});
+
+app.get('/indexer_ner', function(req, res, next) {
+    let raw = require('./cache/raw_ents.json'),
+        m2e = {},
+        e2m = {};
+    //{'mid': 'mails/296997', 'entity': 'Energy Market Report', 'type': 'ORG', 'part': 0},
+    raw.forEach(function(e, i) {
+        if (!(e.mid in m2e)) m2e[e.mid] = {};
+        if (!(e.type in m2e[e.mid])) m2e[e.mid][e.type] = [];
+        if (!(e.entity in e2m)) e2m[e.entity] = [];
+        // TODO make entities type sensitive
+
+        m2e[e.mid][e.type].push(i);
+        e2m[e.entity].push(i);
+    });
+    res.send({
+        list: raw,
+        m2e: m2e,
+        e2m: e2m
+    })
+});
+
+app.get('/indexer_graph', function (req, res, next) {
+    logger.trace('start graphjs');
+    let idlist_query = 'FOR s in sent ' +
+        'LET f = document(s._from) ' +
+        'LET t = document(s._to) ' +
+        'RETURN {m: s.mail_ids[0], fn: f.name, fa: f.address, tn: t.name, ta: t.address, ti:s._to, fi:s._from}';
+
+    config.db.query(
+        idlist_query, {}
+    ).then(function (cursor) {
+        logger.trace('promises arrived');
+        cursor.all().then(function (all) {
+            let m = all.reduce(function (acc, s, i) {
+                if (!(s.m in acc))
+                    acc[s.m] = [];
+                acc[s.m].push({'i': i, fn: s.fn, fa: s.fa, tn: s.tn, ta: s.ta, ti: s.ti, fi: s.fi});
+                return acc;
+            }, {});
+            res.send(m);
+        }).catch(function (err) {
+            logger.error(err.stack);
+            res.status(500).send({error: err.stack});
+        })
+    }).catch(function (err) {
+        logger.error(err.stack);
+        res.status(500).send({error: err.stack});
+    })
 });
 
 //app.use(express.static(config.path('maps/styles/osm-bright')));
